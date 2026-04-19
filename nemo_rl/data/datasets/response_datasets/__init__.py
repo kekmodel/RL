44d1eb019f9f51d5cb4399236c0de9d310ba5e82
ruit <ruit@nvidia.com>
2026-04-19 00:45:11 -0700

    add arrow dataset, xtoken utils
    Signed-off-by: ruit <ruit@nvidia.com>

--- nemo_rl/data/datasets/response_datasets/__init__.py
+++ nemo_rl/data/datasets/response_datasets/__init__.py
@@ -14,6 +14,9 @@
 
 from nemo_rl.data import ResponseDatasetConfig
 from nemo_rl.data.datasets.response_datasets.aime24 import AIME2024Dataset
+from nemo_rl.data.datasets.response_datasets.arrow_text_dataset import (
+    ArrowTextDataset,
+)
 from nemo_rl.data.datasets.response_datasets.clevr import CLEVRCoGenTDataset
 from nemo_rl.data.datasets.response_datasets.daily_omni import DailyOmniDataset
 from nemo_rl.data.datasets.response_datasets.dapo_math import (
@@ -46,6 +49,7 @@ from nemo_rl.data.datasets.response_datasets.tulu3 import Tulu3SftMixtureDataset
 DATASET_REGISTRY = {
     # built-in datasets
     "AIME2024": AIME2024Dataset,
+    "arrow_text": ArrowTextDataset,
     "clevr-cogent": CLEVRCoGenTDataset,
     "daily-omni": DailyOmniDataset,
     "general-conversation-jsonl": GeneralConversationsJsonlDataset,
@@ -95,6 +99,7 @@ def load_response_dataset(data_config: ResponseDatasetConfig):
 
 __all__ = [
     "AIME2024Dataset",
+    "ArrowTextDataset",
     "CLEVRCoGenTDataset",
     "DailyOmniDataset",
     "GeneralConversationsJsonlDataset",
